c6651792682a731cf0ceb8791523f6737f3351fe
Anton Kostyukovich <a.kostyukovich@gmail.com>
2020-05-06 23:16:58 +0300

    first api

--- server/server.js
+++ server/server.js
@@ -4,14 +4,25 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
+// import { fstat } from 'fs'
+// import fs from 'fs'
+const { readFile, writeFile, stat, unlink } = require('fs').promises
 
 let connections = []
 
 const port = process.env.PORT || 3000
 const server = express()
+
+const writeUserFile = async (text) => {
+  await writeFile(`${__dirname}/users.json`, text, { encoding: "utf8" })  
+}
+
+const readUserFile = async () => {
+   await readFile(`${__dirname}/users.json`, { encoding: "utf8" })
+}
 
 server.use(cors())
 
@@ -20,6 +31,71 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+server.use('/api/v1/users', (req, res, next) => {
+  res.set('x-skillcrucial-user', 'bd0cb7a5-0201-4cb2-a65f-32eac42fc872')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+})
+
+server.get('/api/v1/users', async (req, res) => {
+  await stat(`${__dirname}/users.json`)  
+  .then(async () => {
+    await readFile(`${__dirname}/users.json`, { encoding: "utf8" })  
+      .then(data => res.json(JSON.parse(data)))  
+   })
+   .catch(async () => { 
+     await axios('https://jsonplaceholder.typicode.com/users')
+     .then((data) => writeUserFile(JSON.stringify(data.data)))
+     .then(async () => {
+      await readFile(`${__dirname}/users.json`, { encoding: "utf8" })  
+        .then(data => res.json(JSON.parse(data)))  
+     })
+   } )
+   
+})
+
+server.post('/api/v1/users', async (req, res) => {
+  const users = await readFile(`${__dirname}/users.json`, { encoding: "utf8" })  
+  const usersObj = JSON.parse(users)
+  const newId = usersObj.length + 1
+  console.log(newId)
+  usersObj.push({'id': newId})
+  await writeUserFile(JSON.stringify(usersObj))
+  res.json({'status': 'success', 'id': newId})
+  
+})
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const users = await readFile(`${__dirname}/users.json`, { encoding: "utf8" })
+  const usersObj = JSON.parse(users)
+  for (let i = 0; i < usersObj.length; i++){
+    if (usersObj[i].id === +req.params.userId){
+      console.log(usersObj[i].id)
+      usersObj[i] = {...usersObj[i], 'customfield': 'customvalue'}
+    }
+  }
+  await writeUserFile(JSON.stringify(usersObj))
+  res.json({'status': 'success', 'id': req.params.userId})
+  
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const users = await readFile(`${__dirname}/users.json`, { encoding: "utf8" })
+  const usersObj = JSON.parse(users)
+  for (let i = 0; i < usersObj.length; i++){
+    if (usersObj[i].id === +req.params.userId){
+      console.log(usersObj[i].id)
+      usersObj.splice(i, 1)
+    }
+  }
+  await writeUserFile(JSON.stringify(usersObj))
+  res.json({'status': 'success', 'id': req.params.userId})
+})
+
+server.delete('/api/v1/users', async (req, res) => {
+  await unlink(`${__dirname}/users.json`) 
+  res.end()
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
